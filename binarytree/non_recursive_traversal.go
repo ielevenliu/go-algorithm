d9ecec536a74773a0d80547aba0d6c31c8b13c04
liuwenlong2020 <liuwenlong2020@qq.com>
2023-05-17 00:42:39 +0800

    In & Post non recursive traversal

--- binarytree/non_recursive_traversal.go
+++ binarytree/non_recursive_traversal.go
@@ -4,6 +4,7 @@ import (
 	"log"
 )
 
+// 中 -> 左 -> 右
 func PreNonRecursiveTraversal(root *TreeNode) {
 	if root == nil {
 		return
@@ -20,5 +21,49 @@ func PreNonRecursiveTraversal(root *TreeNode) {
 		if node.Left != nil {
 			nodes = append(nodes, node.Left)
 		}
+	}
+}
+
+// 左 -> 中 -> 右
+func InNonRecursiveTraversal(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	log.Printf("In-non-recursive-traversal: ")
+	nodes := []*TreeNode{}
+	for len(nodes) > 0 || root != nil {
+		if root != nil {
+			nodes = append(nodes, root)
+			root = root.Left
+		} else {
+			root = nodes[len(nodes)-1]
+			nodes = nodes[:len(nodes)-1]
+			log.Printf("%d ", root.Value)
+			root = root.Right
+		}
+	}
+}
+
+// 左 -> 右 -> 中
+func PostNonRecursiveTraversal(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	log.Printf("Post-non-recursive-traversal: ")
+	tmpNodes := []*TreeNode{root}
+	ansNodes := []*TreeNode{}
+	for len(tmpNodes) != 0 {
+		node := tmpNodes[len(tmpNodes)-1]
+		tmpNodes = tmpNodes[:len(tmpNodes)-1]
+		ansNodes = append(ansNodes, node)
+		if node.Left != nil {
+			tmpNodes = append(tmpNodes, node.Left)
+		}
+		if node.Right != nil {
+			tmpNodes = append(tmpNodes, node.Right)
+		}
+	}
+	for idx := len(ansNodes) - 1; idx >= 0; idx-- {
+		log.Printf("%d ", ansNodes[idx].Value)
 	}
 }
--- binarytree/non_recursive_traversal_test.go
+++ binarytree/non_recursive_traversal_test.go
@@ -6,3 +6,13 @@ func TestPreNonRecursiveTraversal(t *testing.T) {
 	root := GenBinaryTree()
 	PreNonRecursiveTraversal(root)
 }
+
+func TestInNonRecursiveTraversal(t *testing.T) {
+	root := GenBinaryTree()
+	InNonRecursiveTraversal(root)
+}
+
+func TestPostNonRecursiveTraversal(t *testing.T) {
+	root := GenBinaryTree()
+	PostNonRecursiveTraversal(root)
+}
