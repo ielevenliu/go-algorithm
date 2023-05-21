22b86b52b443978f90cb0b0e91d99feff41eaf56
liuwenlong2020 <liuwenlong2020@qq.com>
2023-05-21 16:08:18 +0800

    matrix

--- other/other.go
+++ other/other.go
@@ -10,18 +10,7 @@ func PrintMatrix(m [][]int) []int {
 	tC, tR, dC, dR := 0, 0, len(m[len(m)-1])-1, len(m)-1
 	for tC <= dC && tR <= dR {
 		ans = append(ans, printEdge(m, tR, tC, dR, dC)...)
-		tC++
-		dC--
-		tR++
-		dR--
-	}
-	for tC <= dC {
-		ans = append(ans, printEdge(m, tR, tC, dR, dC)...)
-		tC++
-		dC--
-	}
-	for tR <= dR {
-		ans = append(ans, printEdge(m, tR, tC, dR, dC)...)
+		tC, tR, dC, dR = tC+1, tR+1, dC-1, dR-1
 	}
 	return ans
 }
@@ -55,4 +44,24 @@ func printEdge(m [][]int, tR, tC, dR, dC int) []int {
 		}
 	}
 	return ans
+}
+
+// 旋转矩阵
+// 解决方案：从外到内按层旋转
+func RotateMatrix(m [][]int) {
+	if len(m) == 0 || len(m[len(m)-1]) == 0 || len(m) != len(m[len(m)-1]) {
+		return
+	}
+	tR, tC, dR, dC := 0, 0, len(m)-1, len(m[len(m)-1])-1
+	for tR <= dR && tC <= dC {
+		rotateEdge(m, tR, tC, dR, dC)
+		tR, tC, dR, dC = tR+1, tC+1, dR-1, dC-1
+	}
+}
+
+func rotateEdge(m [][]int, tR, tC, dR, dC int) {
+	for idx := 0; idx < dC-tC; idx++ {
+		m[tR][tC+idx], m[tR+idx][dC], m[dR][dC-idx], m[dR-idx][tC] =
+			m[dR-idx][tC], m[tR][tC+idx], m[tR+idx][dC], m[dR][dC-idx]
+	}
 }
--- other/other_test.go
+++ other/other_test.go
@@ -4,10 +4,33 @@ import "testing"
 
 func TestPrintMatrix(t *testing.T) {
 	m := [][]int{
+		{1, 2, 3, 4},
+		{4, 5, 6, 6},
+		{7, 8, 9, 9},
+	}
+	t.Logf("Orginal matrix: ")
+	for idx := 0; idx < len(m); idx++ {
+		t.Logf("%+v", m[idx])
+	}
+
+	ans := PrintMatrix(m)
+	t.Logf("Print matrix: %+v", ans)
+}
+
+func TestRotateMatrix(t *testing.T) {
+	m := [][]int{
 		{1, 2, 3},
 		{4, 5, 6},
 		{7, 8, 9},
 	}
-	ans := PrintMatrix(m)
-	t.Logf("matrix: %+v", ans)
+	t.Logf("Orginal matrix: ")
+	for idx := 0; idx < len(m); idx++ {
+		t.Logf("%+v", m[idx])
+	}
+
+	RotateMatrix(m)
+	t.Logf("Rotate matrix: ")
+	for idx := 0; idx < len(m); idx++ {
+		t.Logf("%+v", m[idx])
+	}
 }
